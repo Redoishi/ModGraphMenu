f046b4a9168b449af98a10285f9c88450d482eac
Redoishi <Redoishi@users.noreply.github.com>
2021-01-09 17:13:21 +0100

    Init mod

---
+++ src/main/java/fr/redoishi/modGraphMenu/ModGraphMenu.java
@@ -0,0 +1,16 @@
+package fr.redoishi.modGraphMenu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @author Redoishi
+ */
+public class ModGraphMenu implements ClientModInitializer {
+
+    public static final String MOD_ID = "modgraphmenu";
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
